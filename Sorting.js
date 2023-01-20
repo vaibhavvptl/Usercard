2d7581fb72146781e20e3de04239356f373b0951
vaibhavvptl <vaibhav.patel@techholding.co>
2023-01-20 12:01:29 +0530

    updated for swaping without third number

--- Sorting.js
+++ Sorting.js
@@ -1,25 +1,31 @@
 //to swap two number in array
 //takes first , second  postion and array in which we have  to swap
 const swap = (first, second, arr) => {
-	let temp = arr[first]
-	arr[first] = arr[second]
-	arr[second] = temp
-	// arr[first] = arr[first] + arr[second]
-	// arr[second] = arr[first] - arr[second]
-	// arr[first] = arr[first] - arr[second]
+	// let temp = arr[first]
+	// arr[first] = arr[second]
+	// arr[second] = temp
+
+	//withoot using third variable
+	arr[first] = arr[first] + arr[second]
+	arr[second] = arr[first] - arr[second]
+	arr[first] = arr[first] - arr[second]
 }
 
+//function to sort array in two parts negetive first after postive
 const sortArray = (arrToSort) => {
 	//postion variable to swap negetive number
-	let postion = 0
+	let postionToSwap = 0
 	for (let i = 0; i < arrToSort.length; i++) {
-		//swapung two numbers if current element is negative and incrementing postion variable
-		if (arrToSort[i] < 0) {
-			swap(i, postion++, arrToSort)
+		//if first number is negetive then inceremet postion variable no need for swap
+		if (i === 0 && arrToSort[i] < 0) ++postionToSwap
+
+		//swaping two numbers if current element is negative and incrementing postion variable
+		if (arrToSort[i] < 0 && i > 0) {
+			swap(i, postionToSwap++, arrToSort)
 		}
 	}
 	return arrToSort
 }
 
-const arrayForSorting = [-12, 11, -13, -5, 6, -7, 5, -3, -6]
+const arrayForSorting = [-12, 1, -1, -13, -5, 6, 7, 5, -3, -9]
 console.log(sortArray(arrayForSorting))
--- readme.md
+++ readme.md
@@ -5,3 +5,10 @@ Rearrange the array elements so that all negative numbers appear before all posi
 Example
 Input: -12, 11, -13, -5, 6, -7, 5, -3, -6
 Output: -12 -13 -5 -7 -3 -6 11 6 5
+
+## Solution
+
+used one counter variable for postion to swap in array
+swapped number when cuurent number is negetive
+
+#### not used third variable to swap a number
